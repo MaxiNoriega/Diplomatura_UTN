fd6b1d826c27956fe13864b65484cccfd8c90eec
Maximiliano Noriega <maxinoriega1983@gmail.com>
2024-02-25 18:21:44 +0100

    proyecto m4u1

--- sitio viviendoCostaDelSol/frontend/src/App.js
+++ sitio viviendoCostaDelSol/frontend/src/App.js
@@ -1,16 +1,35 @@
 
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from './componentes/layout/Header';
 import Navegador from './componentes/layout/Navegador';
 import Footer from './componentes/layout/Footer';
 
+import ContactoPage from './pages/ContactoPage';
+import ExcursionesPage from './pages/ExcursionesPage';
+import GastronomiaPage from './pages/GastronomiaPage';
+import HomePage from './pages/HomePage';
+import LugaresPage from './pages/LugaresPage';
+import NosotrosPage from './pages/NosotrosPage';
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Navegador/>
-      <Footer></Footer>
+      <Header />
+
+      <BrowserRouter>
+        <Navegador />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="nostros" element={<NosotrosPage />} />
+          <Route path="gastronomia" element={<GastronomiaPage />} />
+          <Route path="lugares" element={<LugaresPage />} />
+          <Route path="excursiones" element={<ExcursionesPage />} />
+          <Route path="contacto" element={<ContactoPage />} />
+        </Routes>
+      </BrowserRouter>
       
+      <Footer />
     </div>
   );
 }

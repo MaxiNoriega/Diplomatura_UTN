df7e9516de8bba38f98918e169f5b728fd328efc
Maximiliano Noriega <maxinoriega1983@gmail.com>
2024-03-03 13:55:51 +0100

    m4u1

--- sitio viviendoCostaDelSol/frontend/src/App.js
+++ sitio viviendoCostaDelSol/frontend/src/App.js
@@ -2,19 +2,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 /* importo los archivos Header, Navegador y Footoer */
-
-import Header from './componentes/layout/Header';
-import Navegador from './componentes/layout/Navegador';
-import Footer from './componentes/layout/Footer';
+import Header from "./componentes/layout/Header";
+import Navegador from "./componentes/layout/Navegador";
+import Footer from "./componentes/layout/Footer";
 
 /* importo los archivos de las 6 paginas */
 
-import ContactoPage from './pages/ContactoPage';
-import ExcursionesPage from './pages/ExcursionesPage';
-import GastronomiaPage from './pages/GastronomiaPage';
-import HomePage from './pages/HomePage';
-import LugaresPage from './pages/LugaresPage';
-import NosotrosPage from './pages/NosotrosPage';
+import HomePage from "./pages/HomePage";
+import NosotrosPage from "./pages/NosotrosPage";
+import GastronomiaPage from "./pages/GastronomiaPage";
+import LugaresPage from "./pages/LugaresPage";
+import ExcursionesPage from "./pages/ExcursionesPage";
+import ContactoPage from "./pages/ContactoPage";
 
 function App() {
   return (
--- sitio viviendoCostaDelSol/frontend/src/componentes/layout/Header.js
+++ sitio viviendoCostaDelSol/frontend/src/componentes/layout/Header.js
@@ -4,11 +4,11 @@ const Header = (props) => {
     return (
         <Header>
             <div className = "holder">
-                <img src="img/logo.jfif" width="150" alt="logo"></img>
+                <img src=".img/logo.jfif" width="150" alt="logo"></img>
                 <h1>Viviendo Costa del Sol !!!</h1>
             </div>
         </Header>
-    );
+    );  
 }
 
 export default Header;

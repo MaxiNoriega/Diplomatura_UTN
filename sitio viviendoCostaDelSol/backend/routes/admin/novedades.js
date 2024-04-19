e157ddf85c865a53b702b7f52495fde8f00cd3c6
Maximiliano Noriega <maxinoriega1983@gmail.com>
2024-04-19 15:54:58 +0200

    avance proyecto

--- sitio viviendoCostaDelSol/backend/routes/admin/novedades.js
+++ sitio viviendoCostaDelSol/backend/routes/admin/novedades.js
@@ -1,11 +1,47 @@
-var express = require ('express');
+var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
-    res.render ('admin/novedades', {
+//Para listar Novedades
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
+    res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre,
+        novedades
     });
 });
+
+//Para agregar novedades
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {//agregar.hbs
+        layout: 'admin/layout'
+    });//cierra render
+});//cierra Get
+
+//controlador para capturar los datos enviados al formulario
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insetNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true, message: 'No se caro la novedad'
+        })
+    }
+})
 
 module.exports = router;

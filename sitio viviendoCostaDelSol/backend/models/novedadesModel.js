b29fc2aa658dd06b6509bc610e71ac3a64194a97
Maximiliano Noriega <maxinoriega1983@gmail.com>
2024-04-20 17:23:47 +0200

    crud

--- sitio viviendoCostaDelSol/backend/models/novedadesModel.js
+++ sitio viviendoCostaDelSol/backend/models/novedadesModel.js
@@ -6,6 +6,8 @@ async function getNovedades() {
     return rows;
 }
 
+// funcion para agregar novedades a la lista
+
 async function insetNovedad(obj) {
     try {
         var query = "insert into novedades set ? ";
@@ -18,4 +20,31 @@ async function insetNovedad(obj) {
 }//cierro insert
 
 
-module.exports = {getNovedades, insetNovedad}
+//funcion para eliminar novedades de la lista
+async function deleteNovedadesById(id){
+    var query = "delete from novedades where id = ? ";
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+
+/*para traer una sola novedad por el id*/
+async function getNovedadById(id) {
+    var query = 'select * from novedades where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows [0];
+}
+
+/*Para modificar el UPDATE de las novedades*/
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = 'update novedades set ? where id = ?';
+        rows = await pool.query(query, [obj,id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+module.exports = {getNovedades, insetNovedad, deleteNovedadesById, getNovedadById, modificarNovedadById}
--- sitio viviendoCostaDelSol/backend/routes/admin/novedades.js
+++ sitio viviendoCostaDelSol/backend/routes/admin/novedades.js
@@ -14,6 +14,7 @@ router.get('/', async function (req, res, next) {
     });
 });
 
+
 //Para agregar novedades
 router.get('/agregar', (req, res, next) => {
     res.render('admin/agregar', {//agregar.hbs
@@ -21,8 +22,8 @@ router.get('/agregar', (req, res, next) => {
     });//cierra render
 });//cierra Get
 
-//controlador para capturar los datos enviados al formulario
 
+//controlador para capturar los datos enviados al formulario
 router.post('/agregar', async (req, res, next) => {
     try {
         if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
@@ -43,5 +44,51 @@ router.post('/agregar', async (req, res, next) => {
         })
     }
 })
+
+
+/*para eliminar una novedad*/
+router.get('/eliminar/:id', async (req, res, next) => {
+    const id = req.params.id;
+
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+});//cierro get de eliminar
+
+/*modificar: 1 paso => traiga diseño formulario + datos de esa novedad*/
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    console.log(req.params.id);
+    var novedad = await novedadesModel.getNovedadById(id);
+
+    res.render('admin/modificar', {//modificar.hbd => donde me trae la info  a traves de novedad
+        layout: 'admin/layout',
+        novedad
+    })
+});
+
+/*Para modificar la novedad*/
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+
+        console.log(obj)
+
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la movedad'
+        })
+    }
+})
+
 
 module.exports = router;

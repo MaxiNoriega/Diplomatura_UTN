f390214fd4f856c170d6d05a978db19e517b12b0
Maximiliano Noriega <maxinoriega1983@gmail.com>
2024-05-02 13:29:45 +0200

    avance

--- sitio viviendoCostaDelSol/frontend/src/componentes/layout/Navegador.js
+++ sitio viviendoCostaDelSol/frontend/src/componentes/layout/Navegador.js
@@ -12,7 +12,7 @@ const Navegador = (props) => {
                     <li><Link to="/nosotros">Nosotros</Link></li>
                     <li><Link to="/gastronomia">Gastronomia</Link></li>
                     <li><Link to="/lugares">Lugares</Link></li>
-                    <li><Link to="/excursiones">Excursiones</Link></li>
+                    <li><Link to="/novedades">Novedades</Link></li>
                     <li><Link to="/contacto">Contacto</Link></li>
                 </ul>
 
--- sitio viviendoCostaDelSol/frontend/src/pages/ExcursionesPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ExcursionesPage = (props) => { return (
-    <div>ExcursionesPage</div>
-    );
-}
-
-export default ExcursionesPage;
